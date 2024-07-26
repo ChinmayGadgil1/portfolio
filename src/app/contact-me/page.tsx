2c71b18f12f9e535637835e6d345485aa990c949
ChinmayGadgil1 <161110548+ChinmayGadgil1@users.noreply.github.com>
2024-07-26 18:58:08 +0530

    scrollbar fix

--- src/app/contact-me/page.tsx
+++ src/app/contact-me/page.tsx
@@ -1,10 +1,23 @@
 'use client'
-import  {z} from 'zod'
+import { z } from 'zod'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import Navbar from '@/components/Navbar'
+
 const formSchema = z.object({
-    name: z.string().min(2, {
+  name: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
   phone: z.string(),
@@ -18,14 +31,71 @@ function Page() {
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      email:"",
-      phone:"",
+      email: "",
+      phone: "",
     },
   })
 
-  return (
-    <div>
+  const onSubmit = () => {
 
+  }
+
+  return (
+    <div className='w-screen h-screen bg-black text-white'>
+      <Navbar />
+      <div className=' flex justify-center items-center '>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className=" w-1/5 p-4 border border-black rounded-2xl">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className='text-3xl p-3 mt-5'>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your name" className='text-2xl' {...field} />
+                    </FormControl>
+
+
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className='text-3xl p-3 mt-5'>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter your email" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="phone"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className='text-3xl p-3 mt-5'>Contact Number</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter contact number" {...field} />
+                    </FormControl>
+
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        
+      </div>
     </div>
   )
 }
