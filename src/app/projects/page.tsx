c28c8c407802de891329e98165d3a1f9ca363b1f
ChinmayGadgil1 <161110548+ChinmayGadgil1@users.noreply.github.com>
2024-08-09 08:41:55 +0530

    Projects page mobile view updated

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -11,7 +11,9 @@ import {
 
 function page() {
   return (
-    <div className='flex justify-center items-center'>
+    <>
+    
+    <div className='hidden sm:flex justify-center items-center'>
 
       <div className='flex flex-col justify-center items-center gap-5 m-4 w-[60vw] md:w-[40vw]'>
 
@@ -46,6 +48,48 @@ function page() {
 
       </div>
     </div>
+
+
+
+
+    {/* MOBILE VERSION */}
+    <div className='flex w-screen sm:hidden justify-center items-center mt-[40px]'>
+
+      <div className='w-[75vw] flex flex-col justify-center items-center gap-2 m-2  '>
+
+        <div className='flex flex-col justify-center items-center border border-[#702eff] rounded-xl p-2'>
+          <div className='text-6xl font-extrabold p-4'>Sticker-Hive</div>
+          <Carousel className='border p-4 flex justify-center items-center select-none max-w-[50vw]'>
+            <CarouselContent>
+              <CarouselItem className='flex justify-center items-center'><Image src={'/sticker-hive.png'} alt='Sticker Hive' width={400} height={200} /></CarouselItem>
+              <CarouselItem className='flex justify-center items-center'><Image src={'/sticker-hive-2.png'} alt='Sticker Hive' width={400} height={200} /></CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+
+          <div className=' text-xl p-4'>Sticker Hive is your one-stop solution for creating custom keyboard key stickers with ease. Our platform allows you to design and personalize stickers for your keyboard keys without the need for any editing software. 
+          </div>
+          <div >
+          <div className='flex flex-col m-4 items-center'>
+          <p className='text-2xl font-extrabold'>Link:</p>
+          <Link href={'http://sticker-hive.vercel.app/'} className='text-xl font-bold'
+          >http://sticker-hive.vercel.app</Link>
+          </div>
+          <div className='flex flex-col m-4 items-center'>
+          <p className='text-2xl font-extrabold'>Github Repo:</p>
+          <Link href={'https://github.com/ChinmayGadgil1/sticker-hive'} className='text-xl font-bold'
+          >https://github.com/ChinmayGadgil1/sticker-hive</Link>
+          </div>
+          </div>
+          
+        </div>
+
+
+      </div>
+    </div>
+
+    </>
   )
 }
 
