cec3b129bbadef41eb0d07761c90915a49ab7f71
ChinmayGadgil1 <161110548+ChinmayGadgil1@users.noreply.github.com>
2024-08-09 08:41:22 +0530

    Navbar UI fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,16 +27,18 @@ function Navbar() {
         <Link href='/contact-me'  className='flex flex-col justify-center items-center p-2 m-1'><FaPhone/>Contact Me</Link>
       </div>
     </div>
+
+    {/* MOBILE VIEW */}
     <div className="w-screen h-fit bg-[#6a23a8] md:hidden">
       {clicked ?  <Image src={'/cross-svgrepo-com (1).svg'} alt='Burgermenu' width={70} height={70} onClick={handleClick} unoptimized/>: <Image src={'/menu-burger-horizontal-svgrepo-com.svg'} alt='Burgermenu' width={70} height={70} onClick={handleClick} unoptimized/>}
 
       {clicked && <div className='absolute bg-[#6a23a8] flex w-fit rounded-br-xl  items-center'>
 
         <div className='flex flex-col gap-4 text-2xl'>
-        <Link href='/' className='flex justify-start items-center p-2 m-1 gap-3 '><FaHome/>Home</Link>
+        <Link href='/' onClick={handleClick} className='flex justify-start items-center p-2 m-1 gap-3 '><FaHome/>Home</Link>
         
-        <Link href='/projects'  className='flex justify-start items-center p-2 m-1 gap-3'><FaProjectDiagram/>Projects</Link>
-        <Link href='/contact-me'  className='flex justify-start items-center p-2 m-1 gap-3'><FaPhone/>Contact Me</Link>
+        <Link href='/projects' onClick={handleClick} className='flex justify-start items-center p-2 m-1 gap-3'><FaProjectDiagram/>Projects</Link>
+        <Link href='/contact-me' onClick={handleClick} className='flex justify-start items-center p-2 m-1 gap-3'><FaPhone/>Contact Me</Link>
       </div>
         
         </div>}
